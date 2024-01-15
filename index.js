cb10ee0143da387375407ed3dd75cf7663dd4bb6
srivenkat13 <srivenkatsairaj13@gmail.com>
2024-01-15 19:17:44 +0530

    setup a storre

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
+//action creator function
 function oderCake() {
   return {
     type: CAKE_ORDERED,
@@ -8,20 +12,36 @@ function oderCake() {
 }
 
 const initialState = {
-    numOfCakes: 24,
-  };
-  
-  // (previousState, action) => newState
-  
-  const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CAKE_ORDERED:
-        return {
-          numOfCakes: state.numOfCakes - 1,
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
+  numOfCakes: 24,
+};
+
+// (previousState, action) => newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(`Initial State`, store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
+
+store.dispatch(oderCake());
+store.dispatch(oderCake());
+store.dispatch(oderCake());
+
+unsubscribe();
+
+//cannot dispatch an action after unsubscribing
+store.dispatch(oderCake());
